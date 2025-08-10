2b86a14fdc14c81ecca83eb37119829eca5ba96b
Tarun Goel <ezCoder7@gmail.com>
2025-08-10 13:18:45 +0530

    only step1

--- Step-03/Level-01/Q-03.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isSorted(self, arr) -> bool:
-        for i in range(0,len(arr)-1,1):
-            if(arr[i]>arr[i+1]):
-                return False
-        return True
-if __name__== "__main__":
-  obj = Solution()
-  print(obj.isSorted([1, 2, 3, 4, 5,-1]))
